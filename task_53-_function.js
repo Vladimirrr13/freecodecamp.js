72dca9c5397afb5606b05c4e2914fd2c599fe169
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-08-03 22:50:18 +0300

    Update task_83_2-_function.js

--- task_53-_function.js
+++ task_53-_function.js
@@ -916,29 +916,36 @@ Waiting:chainToSwitch("John") should return "" (empty string)
 Waiting:chainToSwitch(156) should return "" (empty string)*/
 
 /* function chainToSwitch(val) {
-  let answer = "";
+  var answer = "";
   // Only change code below this line
-
-switch (val) {
-  case "bob":
-    answer = "Marley";
-    break;
-  case 42:
-    answer = "The Answer";
-    break;
-      case 1:
-    answer = "There is no #1";
-    break;
-      case 99:
-    answer = "Missed me by this much!";
-    break;
-      case 7:
-    answer = "Ate Nine";
-    break;
+  
+  switch(val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
   // Only change code above this line
   return answer;
 }
 
-chainToSwitch(7); */
+console.log(chainToSwitch("bob"));
+console.log(chainToSwitch(42));
+console.log(chainToSwitch(1));
+console.log(chainToSwitch(99));
+console.log(chainToSwitch(7));
+console.log(chainToSwitch("John"));
+console.log(chainToSwitch(156)); */ 
 
 console.log(switchOfStuff(a)); */
