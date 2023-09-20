651095622ef5f673b6e8487a64c307b296042377
Vladimirrr13 <99765660+Vladimirrr13@users.noreply.github.com>
2023-09-20 23:31:28 +0300

    Update task_101.1-_cycles.js

--- task_101-_cycles.js
+++ task_101-_cycles.js
@@ -91,7 +91,21 @@ console.log(total); */
 Ожидание :multiplyAll([[1, 2], [3, 4], [5, 6, 7]])должен вернуться5040
 Ожидание :multiplyAll([[5, 1], [0.2, 4, 0.5], [3, 9]])должен вернуться54 */
 
-/*  */
+/* function multiplyAll(arr) {
+  let product = 1;
+
+  for (let i = 0; i < arr.length; i+=1) {
+    for (let j = 0; j < arr[i].length; j+=1) {
+      product *= arr[i][j]; 
+    }
+  }
+
+  return product;
+}
+
+console.log(multiplyAll([[1], [2], [3]])); 
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]])); 
+console.log(multiplyAll([[5, 1], [0.2, 4, 0.5], [3, 9]]));  */
 
 /* --------------------------------- TASK 107 --------------------------------- */
 
